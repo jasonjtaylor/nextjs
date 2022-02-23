9e33ab3abf1051d44e173b9c4e31ddabed393790
Jason <jtaylor14@athabasca.edu>
2022-02-23 15:50:13 -0400

    Fixed decrement on dislikes.js

--- .next/static/webpack/webpack.4a797d3bd68a9fea.hot-update.js
+++ .next/static/webpack/webpack.4a797d3bd68a9fea.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "4207583126ca270a"; }
+/******/ 	__webpack_require__.h = function() { return "bf429ac9e394452e"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
